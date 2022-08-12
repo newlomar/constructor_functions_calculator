4d015871c3bef76ab0532a83136a1af5da59ba14
Newton Lomar <newtonlomar16@gmail.com>
2022-08-12 15:06:05 -0300

    some implementations

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function Calculadora() {
   this.display = document.querySelector('.display');
+
+  this.pressionaEnter = function () {
+    this.display.addEventListener('keyup', (e) => {
+      if (e.keyCode === 13) {
+        this.realizaConta();
+      }
+    });
+  };
 }

64d27d0d0bc6f4bbc67bef0d5e0efac7e9cee629
Newton Lomar <newtonlomar16@gmail.com>
2022-08-12 18:45:33 -0300

    constructor function done

--- script.js
+++ script.js
@@ -8,4 +8,67 @@ function Calculadora() {
       }
     });
   };
+
+  this.realizaConta = function () {
+    let conta = this.display.value;
+
+    try {
+      conta = eval(conta); // perigoso
+
+      if (!conta) {
+        alert('Conta inválida!');
+        return;
+      }
+
+      this.display.value = String(conta);
+    } catch (e) {
+      alert('Conta inválida!');
+      return;
+    }
+  };
+
+  this.clearDisplay = function () {
+    this.display.value = '';
+  };
+
+  this.apagaUm = function () {
+    this.display.value = this.display.value.split('').slice(0, -1).join('');
+  };
+
+  this.inicia = function () {
+    this.cliqueBotoes();
+    this.pressionaEnter();
+  };
+
+  this.cliqueBotoes = function () {
+    document.addEventListener(
+      'click',
+      function (e) {
+        const el = e.target;
+
+        if (el.classList.contains('btn-num')) {
+          this.btnParaDisplay(el.innerText);
+        }
+
+        if (el.classList.contains('btn-clear')) {
+          this.clearDisplay();
+        }
+
+        if (el.classList.contains('btn-del')) {
+          this.apagaUm();
+        }
+
+        if (el.classList.contains('btn-eq')) {
+          this.realizaConta();
+        }
+      }.bind(this)
+    );
+  };
+
+  this.btnParaDisplay = function (valor) {
+    this.display.value += valor;
+  };
 }
+
+const calculadora = new Calculadora();
+calculadora.inicia();
